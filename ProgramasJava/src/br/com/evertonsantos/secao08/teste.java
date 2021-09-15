4d8369904284899561ed35e0f5bb0abbc03fdb4b
Everton Santos <e.santos081992@gmail.com>
2021-09-15 18:24:14 -0300

    Exercícios Java
    Exercícios java não comitados

---
+++ ProgramasJava/src/br/com/evertonsantos/secao08/teste.java
@@ -0,0 +1,11 @@
+package br.com.evertonsantos.secao08;
+
+import java.util.Scanner;
+
+public class teste {
+
+	public static void main(String[] args) {
+		System.out.println("helloWorld");
+	}
+
+}
